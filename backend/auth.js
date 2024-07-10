2cae28e0186c4b67534ab86b72fd21e6aa468859
Guy Liba <guy33liba@gmail.com>
2024-07-10 17:47:52 +0200

    fasd

--- backend/auth.js
+++ backend/auth.js
@@ -1,7 +1,36 @@
 import express from "express"
 import User from "./models.js"
 import bcrypt from "bcryptjs"
+import jwt from "jsonwebtoken"
 const router = express.Router()
+
+const secretKey = "your-secret-key"
+
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body
+    const user = await User.findOne({ email })
+
+    if (!user) {
+      return res.status(401).json({ error: "Authentication failed try Again" })
+    }
+
+    const passwordMatch = await bcrypt.compare(password, user.password)
+
+    if (!passwordMatch) {
+      return res.status(401).json({ error: "Authentication failed try Again" })
+    }
+
+    // Create a JWT token
+    const token = jwt.sign({ userId: user._id, email: user.email }, secretKey, {
+      expiresIn: "1h",
+    })
+
+    res.status(200).json({ token, userId: user._id })
+  } catch (error) {
+    res.status(500).json({ error: "Authentication failed try Again" })
+  }
+})
 
 router.post("/register", async (req, res) => {
   try {
@@ -9,7 +38,6 @@ router.post("/register", async (req, res) => {
     const hashedPassword = bcrypt.hash(password, 10)
     const user = new User({ name, email, password: hashedPassword })
     await user.save()
-    console.log()
     res.status(200).send({ message: "Success register" })
   } catch (error) {
     res.status(500).send({ message: "Error registering" })
---
+++ backend/middleware.js
@@ -0,0 +1,6 @@
+import jwt from "jsonwebtoken"
+
+const secretKey = "your-secret-key"
+const requireAuth = (req, res, next) => {
+    
+}
